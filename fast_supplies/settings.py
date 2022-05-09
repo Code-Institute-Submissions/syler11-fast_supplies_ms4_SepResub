76b9ca533f86b60222a3071e7c28d40dd13f99d3
syler11 <nemeth.szilard82@gmail.com>
2022-05-09 13:30:15 +0000

    Added procfile

--- fast_supplies/settings.py
+++ fast_supplies/settings.py
@@ -123,12 +123,17 @@ WSGI_APPLICATION = 'fast_supplies.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
@@ -181,3 +186,5 @@ STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 DEFAULT_FROM_EMAIL = 'orders@fastsupplies.co.uk'
+
+ACCOUNT_EMAIL_VERIFICATION = 'none'
