6fad98237f7239db964d13631b1dcdb7194a54eb
syler11 <nemeth.szilard82@gmail.com>
2022-05-10 17:38:20 +0000

    removed secret key form setting

--- fast_supplies/settings.py
+++ fast_supplies/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8sgj7*303_l9fk_u(+fe@(&0+ei)f#($rr_(2@-c6%59)e#xtv'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['fast-supplies.herokuapp.com', 'localhost']
 
