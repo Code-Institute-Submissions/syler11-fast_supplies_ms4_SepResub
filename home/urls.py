00eb3e7c1468bb11ec1f96b407a930b0b635b4af
syler11 <nemeth.szilard82@gmail.com>
2022-05-13 18:02:49 +0000

    Added terms and privacy views url and templates

--- home/urls.py
+++ home/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='home'),
     path('returns/', views.returns, name='returns'),
+    path('terms/', views.terms, name='terms'),
+    path('privacy/', views.privacy, name='privacy'),
 ]
--- home/views.py
+++ home/views.py
@@ -12,3 +12,17 @@ def returns(request):
     A view to return the returns page
     """
     return render(request, "home/returns.html")
+
+
+def terms(request):
+    """
+    A view to return the terms and conditions page
+    """
+    return render(request, "home/terms_and_conditions.html")
+
+
+def privacy(request):
+    """
+    A view to return the privacy page
+    """
+    return render(request, "home/privacy_policy.html")
