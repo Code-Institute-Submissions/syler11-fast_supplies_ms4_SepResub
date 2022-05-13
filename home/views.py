a432bee99a9cdb73c91ba7cfa18b55f44e5d5d4b
syler11 <nemeth.szilard82@gmail.com>
2022-05-13 17:27:17 +0000

    Added return page

--- home/views.py
+++ home/views.py
@@ -6,3 +6,9 @@ def index(request):
     A view to return the index page
     """
     return render(request, "home/index.html")
+
+def returns(request):
+    """
+    A view to return the returns page
+    """
+    return render(request, "home/returns.html")
