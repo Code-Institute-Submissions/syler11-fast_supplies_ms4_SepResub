23128b31e98cfbb2b3b3dff4dd5250c53521d6c4
syler11 <nemeth.szilard82@gmail.com>
2022-05-14 10:11:25 +0000

    Added additional styling on small screens

--- fast_supplies/settings.py
+++ fast_supplies/settings.py
@@ -26,9 +26,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '1')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = True
 
-ALLOWED_HOSTS = ['fast-supplies.herokuapp.com', 'localhost']
+# 'DEVELOPMENT' in os.environ
+
+ALLOWED_HOSTS = []
+
+# 'fast-supplies.herokuapp.com', 'localhost'
 
 
 # Application definition
