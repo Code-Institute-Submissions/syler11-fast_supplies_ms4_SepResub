81629076484f00adb9ff32c6eaad17b693c07403
syler11 <nemeth.szilard82@gmail.com>
2022-06-08 20:24:50 +0000

    Debug = False

--- fast_supplies/settings.py
+++ fast_supplies/settings.py
@@ -35,7 +35,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = False
 
 ALLOWED_HOSTS = ['fast-supplies.herokuapp.com', 'localhost', '127.0.0.1']
 
