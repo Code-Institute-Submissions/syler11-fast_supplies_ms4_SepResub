1922a9100220803cd392403ea2634fde12cec346
syler11 <nemeth.szilard82@gmail.com>
2022-06-08 18:20:44 +0000

    Fixing problems in chomeapp picked up by IDE

--- home/apps.py
+++ home/apps.py
@@ -1,6 +1,12 @@
+"""
+Imports
+"""
 from django.apps import AppConfig
 
 
 class HomeConfig(AppConfig):
+    """
+    Home app configuration class
+    """
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'home'
