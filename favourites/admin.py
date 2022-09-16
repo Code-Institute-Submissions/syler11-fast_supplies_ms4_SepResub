40ab3b8c90ac77d5bff7aac75fee6e1dd289aed3
syler11 <nemeth.szilard82@gmail.com>
2022-09-16 17:42:48 +0000

    used products admin.py as termplate to favourites admin.py and made migrations

--- favourites/admin.py
+++ favourites/admin.py
@@ -1,3 +1,27 @@
+"""
+Imports
+"""
 from django.contrib import admin
+from .models import Favourites
 
 # Register your models here.
+
+
+class FavouritesAdmin(admin.ModelAdmin):
+    """
+    Favourites admin  model class
+    """
+    list_display = (
+        'username',
+    )
+    list_filter = (
+        'username',
+    )
+    search_fields = (
+       'username',
+    )
+
+    ordering = ('username',)
+
+
+admin.site.register(Favourites, FavouritesAdmin)
