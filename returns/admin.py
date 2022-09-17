ed2b6adb8acd8a3c14e3003bcfe97103f83b6fb4
syler11 <nemeth.szilard82@gmail.com>
2022-09-17 11:30:27 +0000

    added returns model and registered in admin

--- returns/admin.py
+++ returns/admin.py
@@ -1,3 +1,33 @@
+"""
+Imports
+"""
 from django.contrib import admin
+from .models import Returns
 
 # Register your models here.
+
+
+class ReturnsAdmin(admin.ModelAdmin):
+    """
+    Favourites admin  model class
+    """
+    list_display = (
+        'username',
+        'return_reason',
+        'return_request_date',
+    )
+    list_filter = (
+        'username',
+        'return_reason',
+        'return_request_date',
+    )
+    search_fields = (
+       'username',
+       'return_reason',
+        'return_request_date',
+    )
+
+    ordering = ('username',)
+
+
+admin.site.register(Returns, ReturnsAdmin)
--- returns/models.py
+++ returns/models.py
@@ -2,6 +2,9 @@
 Imports
 """
 from django.db import models
+from django.contrib.auth.models import User
+
+from checkout.models import Order
 
 
 class Returns(models.Model):
@@ -15,9 +18,21 @@ class Returns(models.Model):
         verbose_name_plural = "Returns"  \
             # Fixing issues in Django admin Returnss
 
+    RETURN_REASON_CHOICES = [
+        ('wrong_item', 'Wrong Item'),
+        ('damaged_item', 'Damaged Item'),
+        ('item_not_received', 'Item Not Received'),
+    ]
+
+    order_number = models.ManyToManyField(Order, blank=True)
+    username = models.OneToOneField(User, on_delete=models.CASCADE)
+    return_reason = models.CharField(max_length=20, choices=RETURN_REASON_CHOICES,)
+    return_request_date = models.DateTimeField(auto_now_add=True)
+    additional_info = models.CharField(max_length=20, blank=True)
+
 
     def __str__(self):
         """
         Return Returns ordernumber name string
         """
-        return self.name
+        return str(self.order_number)
